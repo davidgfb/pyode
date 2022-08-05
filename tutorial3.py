83dbfedd5f2896a1e541e69a3511e64d4982e23c
davidgfb <davidgracia99@gmail.com>
2022-08-05 23:24:37 +0200

    Update tutorial3.py

--- tutorial3.py
+++ tutorial3.py
@@ -15,34 +15,22 @@ from OpenGL.GLUT import glutInit, glutInitDisplayMode,\
      glutPostRedisplay, glutSolidCube
 from ode import World, Body, Mass, GeomBox, collide,\
      ContactJoint, Space, GeomPlane, JointGroup
-from numpy import array
+from numpy import array, ones
 from numpy.linalg import norm
-
-# geometric utility functions
-def scalp (vec, scal):
-    vec = array(vec)
     
-    vec *= scal
-    
-def length (vec):
-    vec = array(vec)
-
-    return norm(vec)
-
 # prepare_GL
 def prepare_GL():
     """Prepare drawing."""
     # Viewport
-    glViewport(0, 0, w, h)
+    glViewport(0, 0, *(i for i in wh))
 
     # Initialize
     glClearColor(0.8, 0.8, 0.9, 0)
     glClear(16640)
-    glEnable(GL_DEPTH_TEST)
-    glDisable(GL_LIGHTING)
-    glEnable(GL_LIGHTING)
-    glEnable(GL_NORMALIZE)
+    tuple(map(glEnable, (GL_DEPTH_TEST, GL_LIGHTING, GL_NORMALIZE)))
     glShadeModel(GL_FLAT)
+
+    w, h = wh
 
     # Projection
     glMatrixMode(GL_PROJECTION)
@@ -54,31 +42,33 @@ def prepare_GL():
     glLoadIdentity()
 
     # Light source
-    glLightfv(GL_LIGHT0, GL_POSITION, (0,0,1,0))
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, (1,1,1,1))
-    glLightfv(GL_LIGHT0, GL_SPECULAR, (1,1,1,1))
+    unos = ones(3)
+    (glLightfv(a) for a in ((GL_LIGHT0, GL_POSITION, (0,0,1)),\
+                            (GL_LIGHT0, GL_DIFFUSE, unos),\
+                            (GL_LIGHT0, GL_SPECULAR, unos)))
+        
     glEnable(GL_LIGHT0)
 
     # View transformation
-    gluLookAt (2.4, 3.6, 4.8, 0.5, 0.5, 0, 0, 1, 0)
+    gluLookAt(2.4, 3.6, 4.8, *(i for i in array((1,1,0)) / 2),\
+              *(i for i in arriba))
 
 # draw_body
 def draw_body(body):
     """Draw an ODE body."""
-    x, y, z = body.getPosition()
-    R = body.getRotation()
-    rot = (R[0], R[3], R[6], 0,
-           R[1], R[4], R[7], 0,
-           R[2], R[5], R[8], 0,
-           x, y, z, 1)
+    rot = list(array(body.getRotation()).reshape(3, 3).\
+               transpose().reshape(9))
+    
+    rot.insert(3, 0)
+    rot.insert(7, 0) #no mape
+    rot.extend((0, *(i for i in body.getPosition()), 1))
 
     glPushMatrix()
 
-    glMultMatrixf(rot)
+    glMultMatrixf(tuple(rot))
 
     if body.shape == "box":
-        sx, sy, sz = body.boxsize
-        glScalef(sx, sy, sz)
+        glScalef(*(i for i in body.boxsize))
         glutSolidCube(1)
 
     glPopMatrix()
@@ -87,14 +77,12 @@ def draw_body(body):
 def create_box(world, space, density, lx, ly, lz):
     """Create a box body and its corresponding geom."""
     # Create body
-    body = Body(world)
-    M = Mass()
+    body, M = Body(world), Mass()
     M.setBox(density, lx, ly, lz)
     body.setMass(M)
 
     # Set parameters for drawing the body
-    body.shape = "box"
-    body.boxsize = (lx, ly, lz)
+    body.shape, body.boxsize = "box", (lx, ly, lz)
 
     # Create a box geom for collision detection
     geom = GeomBox(space, lengths = body.boxsize)
@@ -102,17 +90,19 @@ def create_box(world, space, density, lx, ly, lz):
 
     return body, geom
 
+arriba = (0, 1, 0)
+
 # drop_object
 def drop_object():
     """Drop an object into the scene."""
-    global bodies, geom, counter, objcount
+    global geom, counter, objcount
 
     body, geom = create_box(world, space, 1000, 1, 0.2, 0.2)
-    body.setPosition((gauss(0, 0.1), 3.0, gauss(0, 0.1)))
+    body.setPosition((gauss(0, 0.1), 3, gauss(0, 0.1)))
     theta = uniform(0, 2 * pi)
-    ct = cos(theta)
-    st = sin(theta)
-    body.setRotation((ct, 0, -st, 0, 1, 0, st, 0, ct))
+    ct, st = cos(theta), sin(theta)
+    body.setRotation((ct, 0, -st,  *(i for i in arriba),\
+                      st, 0, ct))
     bodies.append(body)
     geoms.append(geom)
     counter = 0
@@ -123,15 +113,11 @@ def explosion():
     """Simulate an explosion.
     Every object is pushed away from the origin.
     The force is dependent on the objects distance from the origin."""
-    global bodies
-
     for b in bodies:
-        l = b.getPosition()
-        d = length(l)
-        a = max(0, 4e4 * (1 - d ** 2 / 5))
-        l = array(l)
-        l /= (4, 1, 4)
-        scalp(l, a / length(l))
+        l = array(b.getPosition())
+        a = max(0, 4e4 * (1 - norm(l) ** 2 / 5))
+        l /= (4, 1, 4) 
+        l = l / norm(l) * a  
         b.addForce(l)
 
 # pull
@@ -140,33 +126,25 @@ def pull():
     Every object will be pulled back to the origin.
     Every couple of frames there'll be a thrust upwards so that
     the objects won't stick to the ground all the time."""
-    global bodies, counter
-
     for b in bodies:
-        l = list(b.getPosition())
-        scalp(l, -1e3 / length(l))
-        b.addForce(l)
+        l = array(b.getPosition())
+        b.addForce(-array(l) / norm(l) * 1e3)
 
         if counter % 60 == 0:
-            b.addForce((0, 1e4, 0))
+            b.addForce(1e4 * array(arriba))
 
 # Collision callback
 def near_callback(args, geom1, geom2):
     """Callback function for the collide() method.
-
     This function checks if the given geoms do collide and
     creates contact joints if they do."""
     # Check if the objects do collide
-    contacts = collide(geom1, geom2)
-
-    # Create contact joints
-    world,contactgroup = args
-    
-    for c in contacts:
+    # Create contact joints    
+    for c in collide(geom1, geom2):
         c.setBounce(0.2)
         c.setMu(5000)
-        j = ContactJoint(world, contactgroup, c)
-        j.attach(geom1.getBody(), geom2.getBody())
+        ContactJoint(*(i for i in args), c).\
+                         attach(geom1.getBody(), geom2.getBody())
 
 ######################################################################
 
@@ -176,14 +154,15 @@ glutInit()
 # Open a window
 glutInitDisplayMode(GLUT_RGB)
 
-x, y, w, h = 0, 0, 1280, 720
-glutInitWindowPosition(x, y)
-glutInitWindowSize(w, h)
+xy = (0, 0)
+wh = (1280, 720)
+glutInitWindowPosition(*(i for i in xy))
+glutInitWindowSize(*(i for i in wh))
 glutCreateWindow("")
 
 # Create a world object
 world = World()
-world.setGravity(10 * array((0, -1, 0)))
+world.setGravity(-10 * array(arriba))
 world.setERP(0.8)
 world.setCFM(1e-5)
 
@@ -191,26 +170,20 @@ world.setCFM(1e-5)
 space = Space()
 
 # Create a plane geom which prevent the objects from falling forever
-floor = GeomPlane(space, (0, 1, 0), 0)
-
 # A list with ODE bodies
-bodies = []
-
 # The geoms for each of the bodies
-geoms = []
-
 # A joint group for the contact joints that are generated whenever
 # two bodies collide
-contactgroup = JointGroup()
-
 # Some variables used inside the simulation loop
-fps, running, state, counter, objcount, lasttime = 100, True, 0,\
-                                                0, 0, time()
+floor, bodies, geoms, contactgroup, fps, running, state, counter,\
+       objcount, lasttime = GeomPlane(space, arriba, 0), [], [],\
+                            JointGroup(), 100, True, 0, 0, 0,\
+                            time()
 dt = 1 / fps
 
 # keyboard callback
 def _keyfunc (c, x, y):
-    exit(0)
+    exit()
 
 glutKeyboardFunc(_keyfunc)
 
@@ -230,8 +203,7 @@ glutDisplayFunc(_drawfunc)
 def _idlefunc ():
     global counter, state, lasttime
 
-    t = dt - (time() - lasttime) #?
-    print(t)
+    t = dt - time() + lasttime 
 
     if t > 0:
         sleep(t)
@@ -243,8 +215,7 @@ def _idlefunc ():
             drop_object()
 
         if objcount == 30:
-            state = 1
-            counter = 0
+            state, counter = 1, 0
             
     # State 1: Explosion and pulling back the objects
     elif state == 1:
@@ -257,7 +228,7 @@ def _idlefunc ():
         if counter == 500:
             counter = 20
 
-    glutPostRedisplay ()
+    glutPostRedisplay()
 
     # Simulate
     n = 2
